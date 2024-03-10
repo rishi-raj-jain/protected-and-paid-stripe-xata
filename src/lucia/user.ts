09cb68fccf743bacc9722cc24db8dbc6e4273549
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-11 00:45:29 +0530

    cookies

--- src/lucia/user.ts
+++ src/lucia/user.ts
@@ -1,18 +1,21 @@
 import { lucia } from '.'
 import type { User } from 'lucia'
+import type { AstroCookies } from 'astro'
 import { getXataClient, type UserRecord } from '@/xata'
 
-export async function getUser(authCookie: string | undefined): Promise<User | null> {
-  if (!authCookie) return null
-  const sessionId = lucia.readSessionCookie(authCookie)
-  const { user } = await lucia.validateSession(sessionId)
+export async function getUser(cookies: AstroCookies): Promise<User | null> {
+  const auth_session = cookies.get('auth_session')
+  if (!auth_session) return null
+  const session_id = lucia.readSessionCookie(auth_session.value)
+  const { user } = await lucia.validateSession(session_id)
   return user
 }
 
-export async function getUserWithData(authCookie: string | undefined): Promise<UserRecord | null> {
-  if (!authCookie) return null
+export async function getUserWithData(cookies: AstroCookies): Promise<UserRecord | null> {
+  const auth_session = cookies.get('auth_session')
+  if (!auth_session) return null
   const xata = getXataClient()
-  const sessionId = lucia.readSessionCookie(authCookie)
-  const { user } = await lucia.validateSession(sessionId)
+  const session_id = lucia.readSessionCookie(auth_session.value)
+  const { user } = await lucia.validateSession(session_id)
   return await xata.db.user.read(user)
 }
