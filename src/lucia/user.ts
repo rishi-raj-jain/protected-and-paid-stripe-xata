426af1ef1c401a7b586b72490be97333b03907e1
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-10 23:21:16 +0530

    test

--- src/lucia/user.ts
+++ src/lucia/user.ts
@@ -1,9 +1,11 @@
 import { lucia } from '.'
 import type { User } from 'lucia'
+import { getXataClient } from '@/xata'
 
 export async function getUser(authCookie: string | undefined): Promise<User | null> {
   if (!authCookie) return null
+  const xata = getXataClient()
   const sessionId = lucia.readSessionCookie(authCookie)
   const { user } = await lucia.validateSession(sessionId)
-  return user
+  return await xata.db.user.read(user)
 }
