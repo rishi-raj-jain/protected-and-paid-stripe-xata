7ed7cd739d690eff1403f1b2ea23d26e5a8499a2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-10 23:10:59 +0530

    type change

--- src/lucia/index.ts
+++ src/lucia/index.ts
@@ -3,28 +3,19 @@ import { XataAdapter } from './xata'
 
 const adapter = new XataAdapter()
 
-interface DatabaseUser {
-  id: string
-  email: string
-  password: string
-}
-
 export const lucia = new Lucia(adapter, {
   sessionCookie: {
     attributes: {
       secure: import.meta.env.PROD,
     },
   },
-  getUserAttributes: (attributes) => {
-    return {
-      email: attributes.email,
-    }
-  },
 })
 
 declare module 'lucia' {
   interface Register {
     Lucia: typeof lucia
-    DatabaseUserAttributes: Omit<DatabaseUser, 'id'>
+    DatabaseUserAttributes: {
+      email: string
+    }
   }
 }
