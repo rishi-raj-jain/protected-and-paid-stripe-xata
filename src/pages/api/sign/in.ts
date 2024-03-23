1666ea926cfda5b4d686754be4043465a51fc9e2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-23 12:01:27 +0530

    text

--- src/pages/api/sign/in.ts
+++ src/pages/api/sign/in.ts
@@ -5,9 +5,9 @@ import { getXataClient } from '@/xata'
 import type { APIContext } from 'astro'
 import { Argon2id } from 'oslo/password'
 
-export async function POST(context: APIContext): Promise<Response> {
+export async function POST({ request, cookies, redirect }: APIContext): Promise<Response> {
   const xata = getXataClient()
-  const formData = await context.request.formData()
+  const formData = await request.formData()
   const email = (formData.get('email') as string).trim()
   const password = formData.get('password') as string
   const existingRecord = await xata.db.user.filter({ email }).getFirst()
@@ -16,6 +16,6 @@ export async function POST(context: APIContext): Promise<Response> {
   if (!validPassword) return new Response('Incorrect email or password', { status: 400 })
   const session = await lucia.createSession(existingRecord.user_id, {})
   const sessionCookie = lucia.createSessionCookie(session.id)
-  context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
-  return context.redirect('/')
+  cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+  return redirect('/')
 }
--- src/pages/api/sign/up.ts
+++ src/pages/api/sign/up.ts
@@ -6,17 +6,17 @@ import { getXataClient } from '@/xata'
 import type { APIContext } from 'astro'
 import { Argon2id } from 'oslo/password'
 
-export async function POST(context: APIContext): Promise<Response> {
+export async function POST({ request, redirect, cookies }: APIContext): Promise<Response> {
   const xata = getXataClient()
   const user_id = generateId(15)
-  const formData = await context.request.formData()
+  const formData = await request.formData()
   const email = (formData.get('email') as string).trim()
   const password = formData.get('password') as string
   const hashed_password = await new Argon2id().hash(password)
   const existingRecord = await xata.db.user.filter({ email }).getFirst()
   if (existingRecord) {
     if (existingRecord.hashed_password !== null) {
-      return context.redirect('/signin')
+      return redirect('/signin')
     } else {
       await xata.db.user.update(existingRecord.id, { user_id, hashed_password })
     }
@@ -25,6 +25,6 @@ export async function POST(context: APIContext): Promise<Response> {
   }
   const session = await lucia.createSession(user_id, {})
   const sessionCookie = lucia.createSessionCookie(session.id)
-  context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
-  return context.redirect('/')
+  cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+  return redirect('/')
 }
